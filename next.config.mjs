4e563b4dc04e5b9e13d294228e5bfab1aaaeef57
shuaihaoV <158393781+shuaihaoV@users.noreply.github.com.>
2025-01-15 12:23:11 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,25 @@
 /** @type {import('next').NextConfig} */
-const rewrites = () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:8088/api/:path*",
-      },
-    ];
-  };
-const nextConfig = {
-    // output: 'export',
-    rewrites,
-    // outDir: './out'
-};
 
+const nextConfig = async () => {
+  const isDev = process.env.NODE_ENV === "development";
+
+  return {
+    // 在开发模式下关闭 'export' 和 'outDir'
+    ...(isDev ? {} : { output: "export", outDir: "./out" }),
+
+    // 根据环境动态设置 rewrites
+    async rewrites() {
+      if (isDev) {
+        return [];
+      }
+
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://localhost:8088/api/:path*",
+        },
+      ];
+    },
+  };
+};
 export default nextConfig;
