d4303fc03b3c6c0b1898563ffe85118d5059a28a
shuaihaoV <158393781+shuaihaoV@users.noreply.github.com.>
2025-01-15 12:25:22 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,19 @@ const nextConfig = async () => {
   const isDev = process.env.NODE_ENV === "development";
 
   return {
-    // 在开发模式下关闭 'export' 和 'outDir'
-    ...(isDev ? {} : { output: "export", outDir: "./out" }),
+    ...(isDev ? { output: "export", outDir: "./out" }:{}),
 
-    // 根据环境动态设置 rewrites
     async rewrites() {
       if (isDev) {
-        return [];
+        return [
+          {
+            source: "/api/:path*",
+            destination: "http://localhost:8088/api/:path*",
+          },
+        ];
+      }else{
+        return []
       }
-
-      return [
-        {
-          source: "/api/:path*",
-          destination: "http://localhost:8088/api/:path*",
-        },
-      ];
     },
   };
 };
